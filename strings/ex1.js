b1e74b7080a4ee09b23b8d79ef19166485eb24bb
anabergerr <ana.berger@betrybe.com>
2022-04-30 01:47:00 -0300

    source files

---
+++ strings/ex1.js
@@ -0,0 +1,10 @@
+// Write a function that takes two strings (a and b) as arguments
+// Return the number of times a occurs in b
+
+const charactersInString = (string,letra) => {
+    const stringSplit = string.split(letra)
+    return stringSplit
+    //const manyTimesLetter = stringSplit.filter((letter) => letter === letra)
+    //return manyTimesLetter.length
+}
+
