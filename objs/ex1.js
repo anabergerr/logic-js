719d7bd78a19a3b6817406111e6b0aa26ac4356a
anabergerr <ana.berger@betrybe.com>
2022-04-30 16:14:20 -0300

    obj ex1

---
+++ objs/ex1.js
@@ -0,0 +1,9 @@
+// Write a function that takes an object as argument
+// Somehow, the properties and keys of the object got mixed up
+// Swap the Javascript object's key with its values and return the resulting object
+
+const reverseObject = (obj) => {
+    const newObj = {}
+    const keys = Object.keys(obj)
+    
+}
