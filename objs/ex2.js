36baa45f59ad4788f8179f60eb52e44c1c8ef102
anabergerr <ana.berger@betrybe.com>
2022-05-03 00:24:31 -0300

    config jest

---
+++ objs/ex2.js
@@ -0,0 +1,3 @@
+// Write a function that takes two date instances as argument
+// It should return true if the difference between the dates is less than or equal to 1 hour
+// It should return false otherwise
